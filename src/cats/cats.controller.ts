a00a02bf2205f3520cf84da2ec29685f983bc3de
gabrielgoncalvesvitoretti <gabrielvitoretti@icloud.com>
2026-04-17 07:48:06 -0300

    Refactor CatsController to include exception handling and modify create method to accept request body.

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,19 +1,27 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  ForbiddenException,
+  Get,
+  Post,
+  UseFilters,
+} from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { CreateCatDto } from './dto/create-cat.dtos';
-import { Cat } from './interfaces/cat.interface';
-
+import { HttpExceptionFilter } from 'src/http-exception.filter';
 @Controller('cats')
 export class CatsController {
+  service: any;
   constructor(private catsService: CatsService) {}
 
   @Post()
-  create(createCatDto: CreateCatDto) {
+  @UseFilters(new HttpExceptionFilter())
+  create(@Body() createCatDto: CreateCatDto) {
     this.catsService.create(createCatDto);
   }
 
   @Get()
-  findAll(): Cat[] {
-    return this.catsService.findAll();
+  findAll() {
+    throw new ForbiddenException();
   }
 }
